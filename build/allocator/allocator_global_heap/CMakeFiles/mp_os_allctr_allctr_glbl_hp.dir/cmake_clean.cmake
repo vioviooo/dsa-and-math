6bfab1397880447d81dd57f4d66afb198e473fed
Victoria Brovina <117751176+vioviooo@users.noreply.github.com>
2024-04-22 00:34:59 +0300

    develop update allctr glbl hp

---
+++ build/allocator/allocator_global_heap/CMakeFiles/mp_os_allctr_allctr_glbl_hp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp_os_allctr_allctr_glbl_hp.dir/src/allocator_global_heap.cpp.o"
+  "CMakeFiles/mp_os_allctr_allctr_glbl_hp.dir/src/allocator_global_heap.cpp.o.d"
+  "libmp_os_allctr_allctr_glbl_hp.a"
+  "libmp_os_allctr_allctr_glbl_hp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mp_os_allctr_allctr_glbl_hp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
